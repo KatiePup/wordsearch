34546ab8d346d09c6fc4720347ef58a0fb479251
KatiePup <74455738+KatiePup@users.noreply.github.com>
2022-02-22 16:34:04 +0000

    made 2nd level selectable

--- src/components/Wordsearch.vue
+++ src/components/Wordsearch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <table @mouseleave="mouseLeave">
+    <table @mouseleave="stopSelection">
       <thead>
         <tr>
           <th colspan="10">Wordsearch</th>
@@ -10,7 +10,10 @@
         <tr v-for="(row, y) in letterArray" :key="row.ID">
           <td
             class="cell"
-            :class="{ cellSelected: cell.selected }"
+            :class="{
+              cellSelected: cell.selected === 1,
+              cellFound: cell.selected === 2,
+            }"
             v-for="(cell, x) in row"
             :key="cell.ID"
             @click="clickCell(x, y)"
@@ -151,83 +154,96 @@ export default {
           { letter: "J", selected: 0 },
         ],
       ],
-      clicked: {
-        x: -1,
-        y: -1,
+      coord: {
+        x1: -1,
+        y1: -1,
+        x2: -1,
+        y2: -1,
       },
-      hover: {
-        x: -1,
-        y: -1,
-      },
-      direction: {
+      polar: {
         x: 0,
         y: 0,
+        length: 0,
       },
     };
   },
   methods: {
     clickCell: function (x, y) {
-      if (this.clicked.x === -1) {
-        this.clicked.x = x;
-        this.clicked.y = y;
+      if (this.coord.x1 === -1) {
+        this.coord.x1 = x;
+        this.coord.y1 = y;
+      } else {
+        this.select(2);
       }
-      this.calculateSelected();
+      this.selection();
     },
     hoverCell: function (x, y) {
-      this.hover.x = x;
-      this.hover.y = y;
-      this.calculateSelected();
+      this.coord.x2 = x;
+      this.coord.y2 = y;
+      this.selection();
     },
-    mouseLeave: function () {
-      this.clicked.x = -1;
-      this.clicked.y = -1;
-      this.calculateSelected();
+    stopSelection: function () {
+      this.coord.x1 = -1;
+      this.coord.y1 = -1;
+      this.coord.x2 = -1;
+      this.coord.y2 = -1;
+      this.polar.length = 0;
+      this.selection();
     },
     clearSelected: function () {
       this.letterArray.forEach((row) =>
         row.forEach((cell) => {
-          cell.selected = 0;
+          if (cell.selected === 1) {
+            cell.selected = 0;
+          }
         })
       );
     },
-    calculateSelected: function () {
+    selection() {
       this.clearSelected();
-      if (this.clicked.x !== -1) {
-        let x0 = this.clicked.x;
-        let y0 = this.clicked.y;
+      if (this.coord.x1 !== -1) {
+        this.calculateSelected();
+      }
+      this.select(1);
+    },
+    calculateSelected: function () {
+      let x0 = this.coord.x1;
+      let y0 = this.coord.y1;
 
-        let x = this.hover.x - x0;
-        let y = this.hover.y - y0;
+      let x = this.coord.x2 - x0;
+      let y = this.coord.y2 - y0;
 
-        let theta = Math.atan2(y, x);
+      let theta = Math.atan2(y, x);
 
-        if (theta >= Math.PI / 8 && theta < (7 * Math.PI) / 8) {
-          this.direction.y = 1;
-        } else if (theta >= (-7 * Math.PI) / 8 && theta < -Math.PI / 8) {
-          this.direction.y = -1;
-        } else {
-          this.direction.y = 0;
-        }
-        if (theta >= (-3 * Math.PI) / 8 && theta <= (3 * Math.PI) / 8) {
-          this.direction.x = 1;
-        } else if (theta >= (5 * Math.PI) / 8 || theta <= (-5 * Math.PI) / 8) {
-          this.direction.x = -1;
-        } else {
-          this.direction.x = 0;
-        }
-
-        let len = Math.max(Math.abs(x), Math.abs(y)) + 1;
-
-        for (let i = 0; i < Math.max(len, 1); i++) {
-          if (
-            this.letterArray[y0 + this.direction.y * i][
-              x0 + this.direction.x * i
-            ] !== undefined
-          ) {
-            this.letterArray[y0 + this.direction.y * i][
-              x0 + this.direction.x * i
-            ].selected = 1;
-          }
+      if (theta >= Math.PI / 8 && theta < (7 * Math.PI) / 8) {
+        this.polar.y = 1;
+      } else if (theta >= (-7 * Math.PI) / 8 && theta < -Math.PI / 8) {
+        this.polar.y = -1;
+      } else {
+        this.polar.y = 0;
+      }
+      if (theta >= (-3 * Math.PI) / 8 && theta <= (3 * Math.PI) / 8) {
+        this.polar.x = 1;
+      } else if (theta >= (5 * Math.PI) / 8 || theta <= (-5 * Math.PI) / 8) {
+        this.polar.x = -1;
+      } else {
+        this.polar.x = 0;
+      }
+      this.polar.length = Math.max(Math.abs(x), Math.abs(y)) + 1;
+    },
+    select(value) {
+      for (let i = 0; i < this.polar.length; i++) {
+        if (
+          this.letterArray[this.coord.y1 + this.polar.y * i][
+            this.coord.x1 + this.polar.x * i
+          ] !== undefined &&
+          this.letterArray[this.coord.y1 + this.polar.y * i][
+            this.coord.x1 + this.polar.x * i
+          ].selected !== 2
+        ) {
+          this.letterArray[this.coord.y1 + this.polar.y * i][
+            this.coord.x1 + this.polar.x * i
+          ].selected = value;
         }
       }
     },
@@ -261,5 +277,8 @@ a {
 }
 .cellSelected {
   background-color: pink;
+}
+.cellFound {
+  background-color: firebrick;
 }
 </style>
