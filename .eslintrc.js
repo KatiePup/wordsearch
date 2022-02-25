fa8ac2cf3b54676b3e57d5394f5e60f0151a34fd
KatiePup <74455738+KatiePup@users.noreply.github.com>
2022-02-25 12:31:42 +0000

    it doesn't work but it's 1am so am going to bed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 export default {
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
-  plugins: [],
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": "warn",
-    "no-unused-vars": "warn",
-    "spaced-comment": "warn"
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn',
+    'spaced-comment': 'warn'
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module'
   }
-};
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App'
+import App from './App.vue'
 
 const app = createApp(App)
-window.vm = app.mount('#app')
+window.vm = app.mount('#yeet')
